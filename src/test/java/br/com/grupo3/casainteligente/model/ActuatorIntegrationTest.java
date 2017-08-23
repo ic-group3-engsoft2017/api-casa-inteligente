38a9114b383ee64d4904d1e542c56ad29c32adb3
Luiz Henrique Pegoraro <pegorarohluiz@gmail.com>
2017-08-23 01:00:59 -0300

    Commiting IntegrationTests to assure the contract.

---
+++ src/test/java/br/com/grupo3/casainteligente/model/ActuatorIntegrationTest.java
@@ -0,0 +1,19 @@
+package br.com.grupo3.casainteligente.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lpegoraro on 23/08/17.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ActuatorIntegrationTest {
+
+    @Test
+    public void testCRUDOperations() throws Exception {
+
+    }
+}
---
+++ src/test/java/br/com/grupo3/casainteligente/model/ReceiverIntegrationTest.java
@@ -0,0 +1,15 @@
+package br.com.grupo3.casainteligente.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ReceiverIntegrationTest {
+
+    @Test
+    public void testCRUDOperations() throws Exception {
+
+    }
+
+}
