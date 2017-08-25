6ef9f4cd8b42194ce1ab8b81c3c71e950132d456
Jorge Luiz Reis Tavares <jluiz@ciandt.com>
2017-08-25 09:33:33 -0300

    removing commented code

--- src/docker/scripts/configure_env_casa_inteligente.sh
+++ src/docker/scripts/configure_env_casa_inteligente.sh
@@ -6,10 +6,6 @@ apt-get update
 
 _install_mvn339() {
     echo "installing maven..."
-    # wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
-    # tar xzf apache-maven-3.3.9-bin.tar.gz
-    # mv apache-maven-3.3.9 /opt/maven
-    # ln -s /opt/maven/apache-maven-3.3.9/ maven
     apt-get install -y maven
     echo "maven installed"
 }
@@ -25,7 +21,6 @@ _cloning_git_repos() {
     echo "cloning a repository..."
     mkdir -p /home/casainteligente
     git clone -b master https://e41cde3cc67e11b8b4f232aceeeff34512c7b795:x-oauth-basic@github.com/ic-group3-engsoft2017/api-casa-inteligente.git /home/app/casainteligente
-    # e41cde3cc67e11b8b4f232aceeeff34512c7b795
     echo "repository was cloned"
 }
 
--- src/main/java/br/com/grupo3/casainteligente/SmartHouseApplication.java
+++ src/main/java/br/com/grupo3/casainteligente/SmartHouseApplication.java
@@ -1,19 +1,10 @@
 package br.com.grupo3.casainteligente;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ConversionServiceFactoryBean;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.core.convert.converter.Converter;
 
 @SpringBootApplication
 @ComponentScan("smarthouse")
